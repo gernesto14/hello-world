089294ca8b32bb42eafbaf2e1d8be951458a6a5a
gernesto14 <gernesto14@yahoo.com>
2025-03-25 20:51:27 -0400

    added the dotenv config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import "dotenv/config";
+
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000; // Use the assigned port
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! " + `${process.env.TEXT}`);
 });
 
 app.listen(PORT, "0.0.0.0", () => {
